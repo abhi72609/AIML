de848cb7565e3801d4ade22099e050c13891bf46
ABHISHEK KUMAR RAJ <abhishekraj93088@gmail.com>
2026-08-13 15:58:59 +0530

    Update Pandas practice

--- 2_pandas/Day1/Pandas1.py
+++ 2_pandas/Day1/Pandas1.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 
+# Creating Series from numpy arrays ( highlight that pandas is implemented using numpy)
 votes = np.array([120, 240, 150])
 ratings = np.array([4.1, 4.5, 3.9])
 restaurants = np.array(['R1', 'R2', 'R3'])
@@ -10,3 +11,18 @@ print(votes_series)
 
 ratings_series = pd.Series(ratings, index=['R1', 'R2', 'R3'], name='Ratings')
 print(ratings_series)
+
+
+data = {
+    "Restaurant": ["R1", "R2", "R3"],
+    "Votes": [120, 240, 150],
+    "Rating": [4.1, 4.5, 3.9]
+}
+df = pd.DataFrame(data)
+print(df)
+
+
+df = pd.read_csv('C:\\Users\\hp\\Desktop\\Coding\\AIML\\2_pandas\\zomato_dataset.csv')
+print(df.head())
+print(df.tail())
+print(df.tail(2))
