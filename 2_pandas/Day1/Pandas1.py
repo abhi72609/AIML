9444726a4c55b70588cb9438e47239aa03231ece
ABHISHEK KUMAR RAJ <abhishekraj93088@gmail.com>
2026-08-13 17:11:53 +0530

    Update Pandas practice

--- 2_pandas/Day1/Pandas1.py
+++ 2_pandas/Day1/Pandas1.py
@@ -23,6 +23,31 @@ print(df)
 
 
 df = pd.read_csv('C:\\Users\\hp\\Desktop\\Coding\\AIML\\2_pandas\\zomato_dataset.csv')
-print(df.head())
-print(df.tail())
-print(df.tail(2))
+# print(df.head())
+# print(df.tail())
+# print(df.tail(2))
+
+# Basic Operations in rows
+
+# Slicing Rows
+# iloc - Slicing based on implicit integer indices.
+print(df.iloc[0]) 
+print(df.iloc[1:4])  
+# How can Zomato's data team sample every third restaurant from the second to the ninth entry in the
+# Bangalore dataset to explore a subset of restaurant data?
+print(df.iloc[1:9:3])
+
+
+
+
+df_explicit = df.set_index("name")
+print(df_explicit)
+# loc - Slicing based on explicit labels.
+print(df_explicit.loc["Jalsa"])
+print(df_explicit.loc[["Jalsa", "Tuk-Tuk"]])
+
+# You can also reset the index to get back the original df.
+print(df_explicit.reset_index())
+
+# We can drop the index altogether.
+print(df_explicit.reset_index(drop=True))
