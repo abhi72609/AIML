87019f8fa45b61fac61e1261ccc00d4940cf808b
ABHISHEK KUMAR RAJ <abhishekraj93088@gmail.com>
2026-08-14 15:21:57 +0530

    Update Pandas practice

--- 2_pandas/Day1/Pandas1.py
+++ 2_pandas/Day1/Pandas1.py
@@ -55,3 +55,84 @@ print(df_explicit.reset_index())
 print(df_explicit.reset_index(drop=True))
 
 print(df.info())
+print(df.describe())
+
+# Basic Operations on Columns
+# Dot notation
+rating = df.rate  
+print(rating)
+
+# Bracket notation
+costs = df["approx_cost(for two people)"]  
+print(costs)
+
+#accessing multiple columns
+df[['approx_cost(for two people)', 'rate' ]]  
+
+# Renaming columns
+df = df.rename(columns={"approx_cost(for two people)":'cost_for_two',"votes": "Total_Votes", "rate": "Ratings"})
+
+# Displaying renamed DataFrame
+print(df.head())
+
+
+
+# Adding a new column
+df["only_rating"] = df["Ratings"].str[:-2]
+print(df["only_rating"])
+# It is expected to fail! As you can see, the rating is still in object type. We need to convert it into a float type.
+# df["only_rating"] = df["only_rating"].astype(float)
+# print(df["only_rating"])
+# It is failing because it looks like there is an 'N' in the ratings column, which cannot be converted into a float.
+
+# Option 1
+# Replacing certain values in object type columns
+df["only_rating"] = df["only_rating"].replace('N', np.nan)
+df["only_rating"] = df["only_rating"].astype(float)
+print(df["only_rating"])
+
+# Option 2
+# Use pd.to_numeric with errors='coerce'
+df["only_rating"] = pd.to_numeric(df["only_rating"], errors='coerce')
+print(df["only_rating"])
+
+
+print(df["cost_for_two"])
+df["cost_for_two"] = pd.to_numeric(df["cost_for_two"].str.strip("'\""), errors="coerce")
+print(df["cost_for_two"])
+
+
+# Adding a new column
+df['cost_for_one']  = df['cost_for_two'] / 2
+print(df['cost_for_one'])
+
+
+df["is_good"] = df["only_rating"] > 4
+print(df["is_good"])
+
+
+# Unique Values in a Column
+# Finding unique values in the 'location' column
+unique_locations = df["location"].unique()
+print(unique_locations)
+
+
+
+# Value Counts in a Column
+
+# you might want to find out which are the most popular locations that people order from/ dine in
+# How can Zomato's data team count the number of restaurants in each unique location in the
+# Bangalore dataset to identify the most popular dining or ordering areas?
+
+
+# Counting occurrences of each unique value
+location_counts = df["location"].value_counts()
+print(location_counts)
+
+# Which is the most ordered restaurant in the dataset?
+restaurant_order_counts = df["name"].value_counts()
+print(restaurant_order_counts)
+
+# Which type of listing (e.g., delivery, dine-out, etc.) is the most common?
+listing_counts = df['listed_in(type)'].value_counts()
+print(listing_counts)
