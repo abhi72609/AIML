0d4cbdd0404c7a76f3a8b2d284c3f089613c0fe0
ABHISHEK KUMAR RAJ <abhishekraj93088@gmail.com>
2026-08-20 22:19:22 +0530

    Update Pandas practice

--- 2_pandas/Day2/Pandas2.py
+++ 2_pandas/Day2/Pandas2.py
@@ -62,3 +62,64 @@ print("==="*10)
 df_explicit.loc["Jalsa", "flag"] = 0
 print("Updated DataFrame:")
 print(df_explicit)
+
+
+
+# Using .loc for Conditional Updates
+
+# Update 'Rating' for all restaurants with 'Votes' greater than 200
+df_explicit.loc[df_explicit["only_rating"] < 3, "flag"] = -1
+
+
+
+# Combining DataFrames with concat.
+# Creating a second DataFrame
+data2 = {
+    "Restaurant": ["R5", "R6"],
+    "Votes": [90, 110],
+    "Rating": [3.8, 4.3]
+}
+
+df2 = pd.DataFrame(data2)
+
+# Concatenating row-wise
+combined_df = pd.concat([sample_df, df2], ignore_index=True)
+print("Combined DataFrame (Row-wise):")
+print(combined_df)
+
+
+# Creating a DataFrame with new columns
+new_columns = {
+    "Delivery Time (mins)": [30, 25, 35, 20, 40, 28]
+}
+
+df_new_cols = pd.DataFrame(new_columns)
+
+# Concatenating column-wise
+final_df = pd.concat([combined_df, df_new_cols], axis=1)
+print("Final DataFrame (Column-wise):")
+print(final_df)
+
+
+
+# 1. Using `.iloc` to retrieve the first two rows and the last column
+print(sample_df.iloc[:2, -1])
+
+# 2. Using `.loc` to update 'Rating' for restaurants with votes < 150
+sample_df.loc[sample_df["Votes"] < 150, "Rating"] = 4.0
+print(sample_df)
+
+# 3. Adding a new row,
+
+new_row = pd.DataFrame([["R7", 100, 3.7]], columns=sample_df.columns)
+sample_df = pd.concat([sample_df, new_row], ignore_index=True)
+print(sample_df)
+
+# 4. Concatenating two DataFrames
+df_with_time = pd.DataFrame({"Delivery Time (mins)": [30, 40, 20, 35, 25]})
+final_combined_df = pd.concat([sample_df, df_with_time], axis=1)
+print(final_combined_df)
+
+# 5. Finding updated 'Rating' for restaurants with votes > 200
+updated_ratings = final_combined_df.loc[final_combined_df["Votes"] > 200, "Rating"]
+print(updated_ratings)
