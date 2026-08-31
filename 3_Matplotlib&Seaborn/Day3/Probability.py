072be077692f0cc2d80d073a5d11a363ef003b5b
ABHISHEK KUMAR RAJ <abhishekraj93088@gmail.com>
2026-08-31 22:52:25 +0530

    Add Day3 Probability practice

---
+++ 3_Matplotlib&Seaborn/Day3/Probability.py
@@ -0,0 +1,7 @@
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+sachin_data  = pd.read_csv("C:\\Users\\hp\\Desktop\\Coding\\AIML\\3_Matplotlib&Seaborn\\runs,NotOut,mins,bf,fours,sixes,sr,.txt")
+print(sachin_data)
