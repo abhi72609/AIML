d0e9a647e0bad64c295fdbf8142b09988da97ad0
ABHISHEK KUMAR RAJ <abhishekraj93088@gmail.com>
2026-09-08 15:58:20 +0530

    update

---
+++ 4_MathForAIML/01_LineraAlgebra1.py
@@ -0,0 +1,4 @@
+import pandas as pd
+df = pd.read_csv('C:\\Users\\hp\\Desktop\\Coding\\AIML\\4_MathForAIML\\Data1.csv')
+# df = pd.read_csv('Data1.csv')
+print(df.head(10))
---
+++ 5_Supervised_Learnning_ML/1_IntroToML.py
@@ -0,0 +1,4 @@
+import numpy as np
+import pandas as pd
+
+# Load the Cars24 dataset
